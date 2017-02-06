0323b5b5e4faa8f40fe64154d50d7b6b44a01f59
Beeps <Duncan.A.Garrett@gmail.com>
2017-02-06 10:18:14 -0800

    Suppress warning when no page templates exist.
    Prevented utputting "Invalid foreach value" warning to frontend due to
    empty value.

--- classes/creare-reviews-custom-setup.php
+++ classes/creare-reviews-custom-setup.php
@@ -2,69 +2,68 @@
 
 class Creare_Reviews_Custom_Setup
 {
-		/**
-         * Construct the plugin object
-         */
-        public function __construct()
-        {
+	/**
+	 * Construct the plugin object
+	 */
+	public function __construct()
+	{
 
-        	// Get array of options
-            $settings = get_option("clr_settings");
+		// Get array of options
+		$settings = get_option("clr_settings");
 
-        	// check to see if enabled
-            if ( $settings['clr_enablestyling'] == 1) {
-                add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 100 );
-            }
-        	
-        }
+		// check to see if enabled
+		if ( $settings['clr_enablestyling'] == 1) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 100 );
+		}
+		
+	}
 
-        /** 
-         * Enqueue Scripts & Styles
-         */
-        public function enqueue_scripts()
-        {
+	/** 
+	 * Enqueue Scripts & Styles
+	 */
+	public function enqueue_scripts()
+	{
 
-        	// register frontend scripts
-			wp_register_script( 
-				'clr-reviews-bxslider-js', 
-				plugins_url( '/js/bxslider.min.js', dirname( __FILE__ ) ), 
-				array( 'jquery' ), 
-				null
-			);
+		// register frontend scripts
+		wp_register_script( 
+			'clr-reviews-bxslider-js', 
+			plugins_url( '/js/bxslider.min.js', dirname( __FILE__ ) ), 
+			array( 'jquery' ), 
+			null
+		);
 
-			// register frontend styles
-			wp_register_style(
-				'clr-reviews-bxslider-css',
-				plugins_url( 'css/bxslider.css', dirname( __FILE__ ) ),
-				'',
-				null
-			);
+		// register frontend styles
+		wp_register_style(
+			'clr-reviews-bxslider-css',
+			plugins_url( 'css/bxslider.css', dirname( __FILE__ ) ),
+			'',
+			null
+		);
 
-			// register frontend scripts
-			wp_register_script( 
-				'clr-reviews-custom-js', 
-				plugins_url( '/js/custom.js', dirname( __FILE__ ) ), 
-				array( 'jquery' ), 
-				null
-			);
-			// register frontend styles
-			wp_register_style(
-				'clr-reviews-custom-css',
-				plugins_url( 'css/custom.css', dirname( __FILE__ ) ),
-				'',
-				null
-			);
-			
-			// Returns true when 'page-area.php' is being used.
-			//
-			$settings = get_option("clr_settings");
-			$settings = $settings['clr_pagetemplates'];
-
+		// register frontend scripts
+		wp_register_script( 
+			'clr-reviews-custom-js', 
+			plugins_url( '/js/custom.js', dirname( __FILE__ ) ), 
+			array( 'jquery' ), 
+			null
+		);
+		// register frontend styles
+		wp_register_style(
+			'clr-reviews-custom-css',
+			plugins_url( 'css/custom.css', dirname( __FILE__ ) ),
+			'',
+			null
+		);
+		
+		// Returns true when 'page-area.php' is being used.
+		//
+		$settings = get_option("clr_settings");
+		$settings = $settings['clr_pagetemplates'];
+		if (!empty($settings)) {
 			foreach( $settings as $template ) {
 				
-echo $template.'<br/>';
-
-
+				echo $template.'<br/>';
+				
 				if ( is_page_template( $template ) ) {
 					// enqueue frontend styles
 					wp_enqueue_script( 'clr-reviews-bxslider-js' );
@@ -73,9 +72,6 @@ echo $template.'<br/>';
 					wp_enqueue_style( 'clr-reviews-custom-css' );
 				}
 			}
-	
-				
-			//}
-			
-        }
+		}
+	}
 }
